d83fa2033baa192e857ff4e4e68a697c5b638ecc
gman.park <im@gman.io>
2016-11-09 19:35:45 +0900

    [TASK] routing modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import {AboutComponent} from './components/+About/about.component'
 import {AppComponent} from './app.component';
-import {RouterModule, Routes} from "@angular/router";
+import {RouterModule} from "@angular/router";
 
 export const routingModule = RouterModule.forRoot([
     {path: '', redirectTo: 'app', pathMatch: 'full'},
--- src/app/components/+About/about.component.ts
+++ src/app/components/+About/about.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'about',
+    selector: 'content',
     templateUrl: 'about.component.html',
 })
 
